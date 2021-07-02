07f417fd4b88306c8b9f02a0a1cb33ec5beed212
marcos-vinicios13 <80989826+marcos-vinicios13@users.noreply.github.com>
2021-07-02 20:31:44 -0300

    Add files via upload

---
+++ src/sua opnião/suaopnião.js
@@ -0,0 +1,5 @@
+const nome = (prompt("voce gostou do site?"))
+const anoNascimento =parseInt(prompt("voce gosta do manicraft? "))
+const anoatual = parseInt("2021")
+
+alert(`muito obrigado pela sua opnião`)
